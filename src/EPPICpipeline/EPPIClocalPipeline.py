933196294b474c87ead9ec40a574279f6394209e
Kumaran Baskaran <kumar.physics@gmail.com>
2015-01-15 16:10:46 +0100

    Local part finished

--- src/EPPICpipeline/EPPIClocalPipeline.py
+++ src/EPPICpipeline/EPPIClocalPipeline.py
@@ -7,7 +7,7 @@ Created on Dec 19, 2014
 
 from subprocess import call
 from os import system
-from commands import getoutput
+from commands import getstatusoutput
 from subprocess import Popen,PIPE,call
 from string import atoi,atof
 import mysql.connector
@@ -31,13 +31,20 @@ class EppicLocal:
         self.downloadFolder="%s/download"%(self.outdir)
         self.cnx=mysql.connector.connect(user=self.mysqluser,host=self.mysqlhost,password=self.mysqlpasswd)
         self.cursor = self.cnx.cursor()
+        self.fastaFolder="%s/unique_fasta"%(self.outdir)
+        self.uniprotDir="%s/%s"%(self.outdir,self.uniprotDatabase)
     
+    urlUniprotReldataswiss="ftp://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/reldate.txt"
+    urlUniprotReldatamain="ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/reldate.txt"
+    urlUniprotFastaswiss="ftp://ftp.expasy.org/databases/uniprot/current_release/uniref/uniref100/uniref100.fasta.gz"
+    urlUniprotFastamain="ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/uniref/uniref100/uniref100.fasta.gz"
     urlUniprotswiss="ftp://ftp.expasy.org/databases/uniprot/current_release/uniref/uniref100/uniref100.xml.gz"
     urlUniprotmain="ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/uniref/uniref100/uniref100.xml.gz"
     urlTaxonomy="http://www.uniprot.org/taxonomy/?query=*&compress=yes&format=tab"
     urlBlastdbmain="ftp://ftp.uniprot.org/pub" # US main ftp
     urlBlastdbuk="ftp://ftp.ebi.ac.uk/pub" # UK mirror
     urlShiftmapping="ftp://ftp.ebi.ac.uk/pub/databases/msd/sifts/text/pdb_chain_uniprot.lst"
+    
     
     TABLES = {}
     TABLES['uniprot'] = (
@@ -67,6 +74,9 @@ class EppicLocal:
                           "`lineage` text,"
                           "`parent` int"
                           ")")
+    def checkUniprotUrls(self):
+        print "checking"
+    
     def checkMeomory(self):
         if not(self.errorFlg):
             df = Popen(["df",self.outpath], stdout=PIPE)
@@ -97,6 +107,25 @@ class EppicLocal:
             if uniprotDownload:
                 self.errorFlg=True
                 self.errorMsg="ERROR: Can't download uniref100.xml.gz"
+        else:
+            print self.errorMsg
+    
+    def downloadUniprotFasta(self):
+        if not(self.errorFlg):
+            print "Downloading UniProtFasta"
+            uniprotDownload=call(["wget","-q",self.urlUniprotFastaswiss,"-P",self.downloadFolder])
+            if uniprotDownload:
+                self.errorFlg=True
+                self.errorMsg="ERROR: Can't download uniref100.fasta.gz"
+        else:
+            print self.errorMsg
+    def downloadUniprotReldata(self):
+        if not(self.errorFlg):
+            print "Downloading UniProtReldate"
+            uniprotDownload=call(["wget","-q",self.urlUniprotReldataswiss ,"-P",self.downloadFolder])
+            if uniprotDownload:
+                self.errorFlg=True
+                self.errorMsg="ERROR: Can't download reldate"
         else:
             print self.errorMsg
     def downloadTaxonomy(self):
@@ -182,53 +211,162 @@ class EppicLocal:
         self.cnx2=MySQLdb.connect(user=self.mysqluser,host=self.mysqlhost,passwd=self.mysqlpasswd,db=self.uniprotDatabase,local_infile=True)
         self.cursor2=self.cnx2.cursor()
     
-    def uploadUniprotClustersTable(self):
-        if not(self.errorFlg):
-            sqlcmd='''LOAD DATA LOCAL INFILE '%s/uniref100.clustermembers.tab' INTO TABLE uniprot_clusters'''%(self.downloadFolder)
-            try:
-                self.cursor2.execute(sqlcmd)
-            except:
-                self.errorFlg=True
-                self.errorMsg="ERROR: Can't upload uniprot_clusters data"   
-        else:
-            print self.errorMsg 
-    def uploadTaxonomyTable(self):
-        if not(self.errorFlg):
-            sqlcmd='''LOAD DATA LOCAL INFILE '%s/taxonomy-all.tab' INTO TABLE uniprot IGNORE 1 LINES'''%(self.downloadFolder)
-            try:
-                self.cursor2.execute(sqlcmd)
-            except:
-                self.errorFlg=True
-                self.errorMsg="ERROR: Can't upload taxonomy data"   
-        else:
-            print self.errorMsg
-                    
     def uploadUniprotTable(self):
         if not(self.errorFlg):
             sqlcmd='''LOAD DATA LOCAL INFILE '%s/uniref100.tab' INTO TABLE uniprot'''%(self.downloadFolder)
             try:
                 self.cursor2.execute(sqlcmd)
-            except:
+            except MySQLdb.Error, e:
                 self.errorFlg=True
-                self.errorMsg="ERROR: Can't upload uniprot data"   
+                self.errorMsg="ERROR: Can't upload uniprot data"  
+                try:
+                    print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                except IndexError:
+                    print "MySQL Error: %s" % str(e)   
         else:
-            print self.errorMsg   
+            print self.errorMsg
+    
+    def uploadUniprotClustersTable(self):
+        if not(self.errorFlg):
+            sqlcmd='''LOAD DATA LOCAL INFILE '%s/uniref100.clustermembers.tab' INTO TABLE uniprot_clusters'''%(self.downloadFolder)
+            try:
+                self.cursor2.execute(sqlcmd)
+            except MySQLdb.Error, e:
+                self.errorFlg=True
+                self.errorMsg="ERROR: Can't upload uniprot_cluster data"  
+                try:
+                    print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                except IndexError:
+                    print "MySQL Error: %s" % str(e)  
+        else:
+            print self.errorMsg
+             
+    def uploadTaxonomyTable(self):
+        if not(self.errorFlg):
+            sqlcmd='''LOAD DATA LOCAL INFILE '%s/taxonomy-all.tab' INTO TABLE uniprot IGNORE 1 LINES'''%(self.downloadFolder)
+            try:
+                self.cursor2.execute(sqlcmd)
+            except MySQLdb.Error, e:
+                self.errorFlg=True
+                self.errorMsg="ERROR: Can't upload taxonomy data"  
+                try:
+                    print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                except IndexError:
+                    print "MySQL Error: %s" % str(e)   
+        else:
+            print self.errorMsg
+                    
+       
     
     
+    def createUniprotIndex(self):
+        if not(self.errorFlg):
+            print "Indexing uniprot table"
+            sqlcmd="CREATE INDEX UNIPROTID_IDX ON uniprot (uniprot_id)"
+            try:
+                self.cursor2.execute(sqlcmd)
+            except MySQLdb.Error, e:
+                self.errorFlg=True
+                self.errorMsg="ERROR: Can't index uniprot table"  
+                try:
+                    print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                except IndexError:
+                    print "MySQL Error: %s" % str(e)
+        else:
+            print self.errorMsg
     
+    def createUniprotFiles(self):
+        if not(self.errorFlg):
+            #self.uniprotDir="%s/%s"%(self.outdir,self.uniprotDatabase)
+            makedir=call(["mkdir",self.uniprotDir])
+            if makedir:
+                self.errorFlg=True
+                self.errorMsg="ERROR: Can't create %s"%(self.uniprotDir)
+            else:
+                mvfile=call(["mv","%s/uniref100.fasta.gz"%(self.downloadFolder),"%s/"%(self.uniprotDir)])
+                if mvfile:
+                    self.errorFlg=True
+                    self.errorMsg="ERROR: Can't move %s/uniref100.fasta.gz"%(self.downloadFolder)
+                else:
+                    makeblast=getstatusoutput("cd %s;gunzip -c uniref100.fasta.gz | makeblastdb -dbtype prot -logfile makeblastdb.log -parse_seqids -out uniref100.fasta -title uniref100.fasta"%(self.uniprotDir))
+                    if makeblast[0]:
+                        self.errorFlg=True
+                        self.errorMsg="ERROR: Problem in running makeblast"
+                    else:
+                        cpreldate=getstatusoutput("cp %s/reldate.txt %s/"%(self.downloadFolder,self.uniprotDir))
+                        if cpreldate[0]:
+                            self.errorFlg="True"
+                            self.errorMsg="ERROR: Can't copy reldate.txt file to uniprot folder"
+                        
+        else:
+            print self.errorMsg
+    
+    
+    def createUniqueFasta(self):
+        if not(self.errorFlg):
+            print "Creating unique fasta sequences"
+            #self.fastaFolder="%s/unique_fasta"%(self.outdir)
+            makedir=call(["mkdir",self.fastaFolder])
+            if makedir:
+                self.errorFlg=True
+                self.errorMsg="ERROR: Can't create %s"%(self.fastaFolder)
+            else:
+                uniquefasta=call(["java","-Xmx512m","-cp",self.eppicjar,"eppic.tools.WriteUniqueUniprots","-s",\
+                                  "%s/pdb_chain_uniprot.lst"%(self.downloadFolder),"-u",self.uniprotDatabase,"-o",\
+                                  "%s/"%(self.fastaFolder),">","%s/write-fasta.log"%(self.fastaFolder)])
+                if uniquefasta:
+                    self.errorFlg=True
+                    self.errorMsg="ERROR: Creating unique sequences failed" 
+                else:
+                    splitqueries=call(["split","-l","27000","%s/queries.list"%(self.fastaFolder),"queries_"])
+                    mvfiels=getstatusoutput("mv queries_* %s/"%(self.fastaFolder))
+                    if mvfiels[0] or splitqueries:
+                        self.errorFlg=True
+                        self.errorMsg="ERROR: Can't split queries"
+        else:
+            print self.errorMsg
+            
+    
+    def runAll(self):
+        self.checkMeomory()
+        self.downloadUniprot()
+        self.downloadTaxonomy()
+        self.downloadShifts()
+        self.unzipTaxonomy()
+        self.parseUniprotXml()
+        self.createUniprotDatabase()
+        self.createUniprotTables()
+        self.createMysqlConnection()
+        self.uploadUniprotTable()
+        self.uploadUniprotClustersTable()
+        self.uploadTaxonomyTable()
+        self.createUniprotIndex()
+        self.downloadUniprotReldata()
+        self.downloadUniprotFasta()
+        self.createUniprotFiles()
+        self.createUniqueFasta()
         
    
         
 if __name__=="__main__":
-    p=EppicLocal('2015_22','/media/baskaran_k/data/test')
+    p=EppicLocal('2015_01','/media/baskaran_k/data/test')
     #p.checkMeomory()
     #p.downloadUniprot()
     #p.downloadTaxonomy()
     #p.unzipTaxonomy()
     #p.downloadShifts()
     #p.parseUniprotXml()
-    p.createUniprotDatabase()
-    p.createUniprotTables()
-    p.uploadUniprotTable()
+#     p.createUniprotDatabase()
+#     p.createUniprotTables()
+#     p.createMysqlConnection()
+#     p.uploadUniprotTable()
+#     p.uploadUniprotClustersTable()
+#     p.uploadTaxonomyTable()
+    #p.createMysqlConnection()
+    #p.createUniprotIndex()
+    #p.downloadUniprotFasta()
+    #p.downloadUniprotReldata()
+    #p.createUniprotFiles()
+    p.createUniqueFasta()
     print p.errorFlg,p.errorMsg
     
