ee8e132c2c9a4e38a3ed8c677959b4d823756d03
Kumaran Baskaran <kumar.physics@gmail.com>
2015-01-28 10:12:19 +0100

    topup scripts added

---
+++ src/EPPICTopup/UploadTopup.py
@@ -0,0 +1,21 @@
+'''
+Created on Jan 28, 2015
+
+@author: baskaran_k
+'''
+
+
+from time import localtime,strftime
+import sys
+
+class UploadTopup:
+    
+    def __init__(self):
+        print "test"
+        
+        
+    def writeLog(self,msg):
+        t=strftime("%d-%m-%Y_%H:%M:%S",localtime())
+        self.logfile.write("%s\t%s\n"%(t,msg))
+        print "%s\t%s\n"%(t,msg)
+        
